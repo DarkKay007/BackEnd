1cc86dfdf6cb3422c9a5192a75a7057b8213b2c1
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-19 10:53:54 -0500

    Update projectControllers.js

--- app/controllers/projectControllers.js
+++ app/controllers/projectControllers.js
@@ -65,30 +65,44 @@ const createProject = async (req, res) => {
   }
 };
 
-// Actualizar un proyecto
 const updateProject = async (req, res) => {
   const { id } = req.params;
   const updatedProjectData = req.body;
 
   try {
+    console.log(`Received update request for project ID: ${id}`);
+    console.log('Updated project data:', updatedProjectData);
+
     const project = await projectCollection.findOne({ _id: new ObjectId(id) });
 
     if (!project) {
+      console.log('Project not found');
       return res.status(404).send('Project not found');
     }
 
     // Verificar permisos
     if (req.user.rol !== 'Administrador' && project.createdBy !== req.user._id.toString()) {
+      console.log('Access denied for user:', req.user._id);
       return res.status(403).json({ message: 'Access denied' });
     }
 
-    await projectCollection.updateOne({ _id: new ObjectId(id) }, { $set: updatedProjectData });
+    const updateResult = await projectCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updatedProjectData }
+    );
+
+    if (updateResult.modifiedCount === 0) {
+      console.log('No changes made to the project');
+      return res.status(304).send('No changes made to the project');
+    }
+
     res.status(200).send('Project updated successfully');
   } catch (error) {
     console.error(`Error updating project: ${error}`);
     res.status(500).send('Internal Server Error');
   }
 };
+
 
 // Eliminar un proyecto
 const deleteProject = async (req, res) => {
