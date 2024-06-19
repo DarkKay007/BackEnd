af346a933349e0507f4bf15a1737d74344fb3a5a
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-18 23:18:35 -0500

    Update projectsRoutes.js

--- app/routes/projectsRoutes.js
+++ app/routes/projectsRoutes.js
@@ -7,7 +7,7 @@ const projectRoutes = express.Router();
 projectRoutes.post('/projects', authorize(['Usuario', 'Administrador']), createProject);
 projectRoutes.get('/projects', authorize(['Usuario', 'Administrador']), getAllProjects);
 projectRoutes.get('/projects/:id', authorize(['Usuario', 'Administrador']), getProjectById);
-projectRoutes.put('/projects/:id', authorize([['Usuario', 'Administrador']]), updateProject);
+projectRoutes.put('/projects/:id', authorize(['Usuario', 'Administrador']), updateProject); // Ajuste aquí
 projectRoutes.delete('/projects/:id', authorize(['Usuario', 'Administrador']), deleteProject);
 projectRoutes.put('/projects/:id/assign', authorize(['Usuario', 'Administrador']), assignUsersToProject);
 
